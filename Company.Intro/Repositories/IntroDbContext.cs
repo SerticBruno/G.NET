22c4aac93ea275935abe3b3813f018c504d64b83
Bruno Sertić <bruno.sertic@rokolabs.com>
2023-10-17 11:28:27 +0200

    created introdbcontext

--- Company.Intro/Repositories/IntroDbContext.cs
+++ Company.Intro/Repositories/IntroDbContext.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Reflection.Metadata;
+using Company.Intro.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Company.Intro.Repositories
 {
     public class IntroDbContext : DbContext
     {
+        public List<User>? Users { get; set; }
+
+        public IntroDbContext(DbContextOptions<IntroDbContext> options)
+            : base(options)
+        {
+        }
     }
 }
