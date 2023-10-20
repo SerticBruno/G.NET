39672085745249e805499809ee46804929a443a5
Bruno Sertić <bruno.sertic@rokolabs.com>
2023-10-21 00:29:50 +0200

    changed list of users to dbset added create user method signature

--- Company.Intro/Repositories/IntroDbContext.cs
+++ Company.Intro/Repositories/IntroDbContext.cs
@@ -6,7 +6,7 @@ namespace Company.Intro.Repositories
 {
     public class IntroDbContext : DbContext
     {
-        public List<User>? Users { get; set; }
+        public DbSet<User>? Users { get; set; }
 
         public IntroDbContext(DbContextOptions<IntroDbContext> options)
             : base(options)
