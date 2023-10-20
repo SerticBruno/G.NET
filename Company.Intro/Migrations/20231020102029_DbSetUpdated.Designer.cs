1ee6edaa23c590a733536492fa1843c87990d280
Bruno Sertić <bruno.sertic@rokolabs.com>
2023-10-21 00:35:15 +0200

    updated migrations

--- Company.Intro/Migrations/20231020102029_DbSetUpdated.Designer.cs
+++ Company.Intro/Migrations/20231020102029_DbSetUpdated.Designer.cs
@@ -4,6 +4,7 @@ using Company.Intro.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Company.Intro.Migrations
 {
     [DbContext(typeof(IntroDbContext))]
-    partial class IntroDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231020102029_DbSetUpdated")]
+    partial class DbSetUpdated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
