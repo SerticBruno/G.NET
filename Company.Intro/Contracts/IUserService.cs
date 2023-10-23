168ea85b8b4205e246db1394b6248d65c0b73ff4
Bruno Sertić <bruno.sertic@rokolabs.com>
2023-10-23 11:16:42 +0200

    changed users to nullable in contract

--- Company.Intro/Contracts/IUserService.cs
+++ Company.Intro/Contracts/IUserService.cs
@@ -5,7 +5,7 @@ namespace Company.Intro.Contracts
 {
     public interface IUserService
     {
-        public DbSet<User> Users { get; set; }
+        public DbSet<User>? Users { get; set; }
         Task<User> CreateUserAsync(User user);
         Task<User> UpdateUserAsync(User user);
     }
